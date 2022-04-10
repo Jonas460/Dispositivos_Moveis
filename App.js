9da169177d058cbc3fa582a05a32fd4b188e8dd8
Jonas456 <jonasa460@gmail.com>
2022-04-09 22:42:42 -0300

    feature: Prova  Questão 1

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const Separator = () => (
+    <View style={styles.separator} />
+  );
+
+  const text = (counter === 1) ? `vez` : `vezes`
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >Você clicou {counter} {text}</Text>
+      <Separator />
+      <Button
+        onPress={() => setCounter(counter + 1)}
+        title='Clique' />
+      <Separator />
+      <Button
+        onPress={() => setCounter(0)}
+        title='Limpar' />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    marginTop: '10px'
+  }
 });
