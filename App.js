7c725b068baac883cc103e76c42bebb5c6aa8cf0
Jonas460 <jonasa460@gmail.com>
2022-04-10 01:14:21 -0300

    feature: Questão 2

--- App.js
+++ App.js
@@ -1,42 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useState } from 'react';
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  const Separator = () => (
-    <View style={styles.separator} />
-  );
-
-  const text = (counter === 1) ? `vez` : `vezes`
-
   return (
     <View style={styles.container}>
-      <Text >Você clicou {counter} {text}</Text>
-      <Separator />
-      <Button
-        onPress={() => setCounter(counter + 1)}
-        title='Clique' />
-      <Separator />
-      <Button
-        onPress={() => setCounter(0)}
-        title='Limpar' />
-      <StatusBar style="auto" />
+      <View style={styles.blueSquare}>
+        <Text style={styles.text}>Square 1</Text>
+      </View>
+      <View style={styles.greenSquare}>
+        <Text style={styles.text}>Square 2</Text>
+      </View>
+      <View style={styles.redSquare}>
+        <Text style={styles.text}>Square 3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  text: {
+    marginTop: 40,
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  blueSquare: {
+    backgroundColor: '#0D58DA',
+    flex: 1,
+    aspectRatio: 1,
+    marginRight: 10,
+    marginLeft: 10,
+  },
+  greenSquare: {
+    backgroundColor: '#398a00',
+    flex: 1,
+    aspectRatio: 1,
+    marginRight: 10,
+  },
+  redSquare: {
+    backgroundColor: '#ED2C24',
+    flex: 1,
+    aspectRatio: 1,
+    marginRight: 10,
   },
 });
