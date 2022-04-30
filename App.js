3ee01ba382d0fbff1448955c2dbb0de01d0573ee
Jonas460 <jonasa460@gmail.com>
2022-04-30 20:05:05 -0300

    questao1

--- App.js
+++ App.js
@@ -1,42 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useState } from 'react';
+import React from "react";
+import { dark } from "./src/theme";
+import Home from "./src/screens/Home";
+import { ThemeProvider } from "styled-components";
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  const Separator = () => (
-    <View style={styles.separator} />
-  );
-
-  const text = (counter === 1) ? `vez` : `vezes`
-
   return (
-    <View style={styles.container}>
-      <Text >Você clicou {counter} {text}</Text>
-      <Separator />
-      <Button
-        onPress={() => setCounter(counter + 1)}
-        title='Clique' />
-      <Separator />
-      <Button
-        onPress={() => setCounter(0)}
-        title='Limpar' />
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={dark}>
+      <Home />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
